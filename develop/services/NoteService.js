a048a19e4d9ed764328a8bbaa11a99421ce5b924
alexbrandon3 <alexlbrandon@gmail.com>
2021-10-25 10:33:30 -0400

    creation of NoteService to remove the clutter of methods from server.js

--- develop/services/NoteService.js
+++ develop/services/NoteService.js
@@ -1,24 +1,25 @@
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class NoteService {
-    getNotes(){
-       return this.readFile().then((data) =>{
-            return JSON.parse(data);
-        })
-    }
-    // helper functions
-    readFile(){
-        return readFileAsync(path.join(__dirname, '../db/db.json'), 'utf8')
-    }
-    writeFile(notes){
-        return writeFileAsync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes))
-    }
+  getNotes() {
+    return this.readFile().then((data) => {
+      return JSON.parse(data);
+    });
+  }
+  // helper functions
+  readFile() {
+    return readFileAsync(path.join(__dirname, "../db/db.json"), "utf8");
+  }
+  writeFile(notes) {
+    return writeFileAsync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notes)
+    );
+  }
 }
 
-
-
-module.exports = new NoteService()
+module.exports = new NoteService();

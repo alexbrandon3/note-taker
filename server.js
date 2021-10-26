5b56ee713f643911fd64fed6ac5c433154b16e72
alexbrandon3 <alexlbrandon@gmail.com>
2021-10-26 18:23:19 -0400

    added start script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const htmlRoutes = require('../../../routes/htmlRoutes')
-const apiRoutes = require('../../../routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 
 
 const PORT = process.env.PORT || 3001;
